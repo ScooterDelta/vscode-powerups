692f15bce050c9aaa2a6a8ee9af83d7889e51f0f
Anthony Farquharson <scooterdelta@gmail.com>
2023-01-19 17:49:28 +0000

    Adds formatting for readmes

---
+++ docs/00-manual/README.md
@@ -0,0 +1,2 @@
+# My Awesome App
+
--- worker/Program.cs
+++ worker/Program.cs
@@ -1,5 +1,6 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
