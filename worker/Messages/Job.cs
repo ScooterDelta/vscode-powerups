764cec0a2f8ecfe28a430538f2f03731311c06ea
Anthony Farquharson <scooterdelta@gmail.com>
2023-01-21 22:13:41 +0000

    Adds dotnet listener

---
+++ worker/Messages/Job.cs
@@ -0,0 +1,7 @@
+namespace Messages
+{
+  public class Job
+  {
+    public int JobNumber { get; set; }
+  }
+}
--- worker/Program.cs
+++ worker/Program.cs
@@ -1,7 +1,46 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+using Messages;
+using Rebus.Activation;
+using Rebus.Config;
 
-WebApplication app = builder.Build();
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingContext, configuration) =>
+    {
+      configuration.Sources.Clear();
 
-app.MapGet("/", () => "Hello World!");
+      IHostEnvironment env = hostingContext.HostingEnvironment;
 
-app.Run();
+      _ = configuration
+          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+          .AddEnvironmentVariables();
+
+      IConfigurationRoot configurationRoot = configuration.Build();
+
+      string rabbitEndpoint = configurationRoot.GetValue<string>("RabbitConnectionString");
+
+      using BuiltinHandlerActivator adapter = new();
+      if (string.IsNullOrEmpty(rabbitEndpoint))
+      {
+        throw new Exception("Invalid RabbitMQ Connection string - no value provided");
+      }
+
+      _ = adapter.Handle<Job>(async job =>
+      {
+        Console.WriteLine("Processing job {0}", job.JobNumber);
+
+        await Task.Delay(200);
+      });
+
+      _ = Configure.With(adapter)
+          .Logging(l => l.ColoredConsole(Rebus.Logging.LogLevel.Warn))
+          .Transport(t => t.UseRabbitMq(rabbitEndpoint, configurationRoot.GetValue<string>("RabbitQueueName")))
+          .Options(o => o.SetMaxParallelism(5))
+          .Start();
+
+      adapter.Bus.Subscribe<Job>().Wait();
+
+      Console.WriteLine("Consumer listening");
+    })
+    .Build();
+
+await host.RunAsync();
